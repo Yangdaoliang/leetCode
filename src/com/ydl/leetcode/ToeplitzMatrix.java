0286e10bf55ac7af2c8f8ff83a51d6875e7361fd
ydl <1524871043@qq.com>
2018-04-09 00:16:30 +0800

    766

---
+++ src/com/ydl/leetcode/ToeplitzMatrix.java
@@ -0,0 +1,18 @@
+package com.ydl.leetcode;
+
+public class ToeplitzMatrix {
+
+    public static boolean isToeplitzMatrix(int[][] matrix) {
+        int xLength = matrix.length;
+        int yLength = matrix[0].length;
+        for (int i = 0;i < xLength-1;i++){
+            for(int j = 0;j < yLength-1;j++){
+                if(matrix[i][j] != matrix[i+1][j+1]){
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
