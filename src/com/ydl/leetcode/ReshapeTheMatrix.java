ac9f616753244f78fdb3e63dd306a85b60597ef0
ydl <1524871043@qq.com>
2018-04-09 23:30:20 +0800

    566

---
+++ src/com/ydl/leetcode/ReshapeTheMatrix.java
@@ -0,0 +1,20 @@
+package com.ydl.leetcode;
+
+public class ReshapeTheMatrix {
+
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        //先将二维数组拉成一维数组。然后在进行数组位置转换。
+        int row = nums.length;
+        int colnum = nums[0].length;
+        int n = row * colnum;
+        if (n == r * c){
+            int[][] res = new int[r][c];
+            for (int i = 0;i < n;i++){
+                res[i/c][i%c] = nums[i/colnum][i%colnum];
+            }
+            return res;
+        }else{
+            return nums;
+        }
+    }
+}
