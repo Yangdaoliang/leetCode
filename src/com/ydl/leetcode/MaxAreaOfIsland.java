13c98961e5a0e4cfa2ec644fdb4a9a149d5e1e7e
ydl <1524871043@qq.com>
2018-05-02 22:31:42 +0800

    695

---
+++ src/com/ydl/leetcode/MaxAreaOfIsland.java
@@ -0,0 +1,33 @@
+package com.ydl.leetcode;
+
+public class MaxAreaOfIsland {
+
+    /**
+     * 求岛屿中的最大值。
+     * 1、求相邻的1的最大个数。
+     * 2、避免重复计算。将走过的数字变成0。
+     */
+
+    public int maxAreaOfIsland(int[][] grid) {
+
+        int maxValue = 0;
+
+        for(int i = 0;i < grid.length;i++){
+            for (int j = 0;j < grid[0].length;j++){
+                maxValue = Math.max(maxValue,dfs(grid,i,j));
+            }
+        }
+        return maxValue;
+    }
+
+    public int dfs(int[][] grid,int i,int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){
+            return 0;
+        }
+        int tempMaxValue = 1;
+        grid[i][j] = 0;
+        tempMaxValue += dfs(grid,i,j-1) + dfs(grid,i,j+1)+dfs(grid,i-1,j)+dfs(grid,i+1,j);
+
+        return tempMaxValue;
+    }
+}
