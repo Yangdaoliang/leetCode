5eb818e689dee6776a3f4c090a01a18a704f02bd
ydl <1524871043@qq.com>
2018-05-02 22:56:25 +0800

    695

---
+++ src/com/ydl/leetcode/FindDisappearedNumbers.java
@@ -0,0 +1,34 @@
+package com.ydl.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDisappearedNumbers {
+    /**
+     * 标志位方法
+     * 1、将这些数字本来应该出现的位置标记为负数
+     * 2、找寻没有出现负数的位置的数
+     */
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> res = new ArrayList<>();
+        //判空处理一下
+        if (nums == null || nums.length == 0){
+            return res;
+        }
+        int n = nums.length;
+        for (int i = 0;i < n;i++){
+            int val = Math.abs(nums[i]) - 1;
+            if (nums[val] > 0){
+                nums[val] = -nums[val];
+            }
+        }
+        for (int i = 0;i < n;i++){
+            if (nums[i] > 0){
+                res.add(i+1);
+            }
+        }
+        return res;
+    }
+
+}
